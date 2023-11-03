0afc609139019c667854e948bea23627d1bbe475
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2023-11-03 18:08:09 +0800

    up a commit to main fix

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -19,6 +19,8 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
+    protected static ?string $navigationLabel = 'Users & Roles';
+
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
