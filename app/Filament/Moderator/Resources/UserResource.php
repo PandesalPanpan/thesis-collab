6f86fb8b6bd4832542a1d13d47bab2ab6427674e
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2024-01-18 10:11:00 +0800

    Label for the Action Button to "Assign and Edit"

--- app/Filament/Moderator/Resources/UserResource.php
+++ app/Filament/Moderator/Resources/UserResource.php
@@ -56,7 +56,8 @@ class UserResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\EditAction::make()
+                    ->label('Assign & Edit'),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
