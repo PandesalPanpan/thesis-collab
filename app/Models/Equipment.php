8f4c1974fb1a4b406ca8db2e6b6a956bba7635c9
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2024-01-29 08:22:57 +0800

    implements hasMedia for image upload

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -5,11 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Equipment extends Model
+class Equipment extends Model implements HasMedia
 {
     use HasFactory;
-
+    use InteractsWithMedia;
+    protected $fillable = [
+        'name','user_id'
+    ];
+    // public function registerMediaConversions(Media $media = null): void
+    // {
+    //     $this->addMediaConversion('thumb')
+    //           ->width(200)
+    //           ->height(200);
+    //           //->sharpen(10);
+    // }
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
