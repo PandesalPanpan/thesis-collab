b12b710b555cd96dceea72ea77a493750b5de35c
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2024-01-31 17:34:32 +0800

    Itech Title page

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,7 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
  
-        <title>{{ config('app.name') }}</title>
+        <title>ITech Laboratory Equipment</title>
  
         <style>
             [x-cloak] {
