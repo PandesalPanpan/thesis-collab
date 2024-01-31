fad5228e00a3c6179047f13f550bde4ba914e7b0
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2024-01-31 17:14:08 +0800

    changed icon

--- app/Filament/Moderator/Resources/UserResource.php
+++ app/Filament/Moderator/Resources/UserResource.php
@@ -19,7 +19,7 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-user-circle';
 
     public static function form(Form $form): Form
     {
