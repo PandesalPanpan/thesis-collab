c4e99393632ea6ea3812d1831bf7d68d73991701
Mozorella <juanito.mozo.iv@gmail.com>
2024-02-14 22:42:50 +0800

    modified:   app/Http/Controllers/EquipmentController.php

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Filament\Notifications\Notification;
 use App\Models\Equipment;
+use App\Models\Log;
 
 class EquipmentController extends Controller
 {
-    public function store (Request $request)
+    public function store(Request $request)
     {
         $equipment = new Equipment();
         $equipment->name = $request->name;
@@ -31,13 +32,59 @@ class EquipmentController extends Controller
         if ($rfidTag) {
             // Check if the tag is allowed
             if ($rfidTag->status) {
+                // Create logs entry
+                $logs = new log();
+                $logs->equipment_id = $rfidTag->id;
+                $logs->status = 1; // Permission granted
+                $logs->save();
+
                 return response()->json(['message' => 'Permission granted'], 200);
             } else {
+                // Create logs entry
+                $logs = new log();
+                $logs->equipment_id = $rfidTag->id;
+                $logs->status = 0; // Permission denied
+                $logs->save();
+
                 return response()->json(['message' => 'Permission denied'], 403);
             }
         } else {
             return response()->json(['message' => 'Tag not found'], 404);
-            
         }
     }
+
+
 }
+
+/*public function checkPermission(Request $request)
+{
+    $tag = $request->input('rfid');
+
+    // Find the RFID tag in the database
+    $rfidTag = Equipment::where('rfid', $tag)->first();
+
+    if ($rfidTag) {
+        // Check if the tag is allowed
+        if ($rfidTag->status) {
+            return response()->json(['message' => 'Permission granted'], 200);
+            $logs = new logs();
+            $logs->equipment_id = $rfidTag->id;
+            $logs->user_id = $request->user_id;
+            $logs->created_at = now();
+            $logs->status = $staus;
+            $logs->save();
+        } else {
+            return response()->json(['message' => 'Permission denied'], 403);
+            $logs = new logs();
+            $logs->equipment_id = $rfidTag->id;
+            $logs->user_id = $request->user_id;
+            $logs->created_at = now();
+            $logs->status = $staus;
+            $logs->save();
+        }
+    } else {
+        return response()->json(['message' => 'Tag not found'], 404);
+        
+    }
+}
+}*/
