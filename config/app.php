b9c6a20105144f4260b08a45b4f3d72bf541a03d
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2024-02-18 13:50:09 +0800

    commeted app panel provider

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\Filament\AdminPanelProvider::class,
-        App\Providers\Filament\AppPanelProvider::class,
+        // App\Providers\Filament\AppPanelProvider::class,
         App\Providers\Filament\ModeratorPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
