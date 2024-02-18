26928190ee9d43117f0d0d7163a8ae6a9e039bd5
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2024-02-18 13:50:25 +0800

    updated comment and href link to moderator panel

--- resources/views/livewire/list-equipments.blade.php
+++ resources/views/livewire/list-equipments.blade.php
@@ -1,7 +1,9 @@
 <div class="wrapper w-full md:max-w-5xl mx-auto pt-20 px 4">
     <h1 class="text-xl font-medium">Laboratory Equipments</h1>
+    <!-- If ever nasa merge conflict, delete this, prioritize yung kay Jam
+    pero I change ung href ni jam into "/moderator" din katulad nung nandito -->
     <x-filament::button
-        href="/app"
+        href="/moderator"
         tag="a"
     >
         Access Panel
