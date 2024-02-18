b3dd7c4830d74d8d39eaec5d034295fe9938fcc1
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2024-02-18 13:57:01 +0800

    changed Navigation Label to "Users & Borrow" and appears second in navigation

--- app/Filament/Moderator/Resources/UserResource.php
+++ app/Filament/Moderator/Resources/UserResource.php
@@ -22,6 +22,8 @@ class UserResource extends Resource
     protected static ?string $model = User::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-user-circle';
+    protected static ?string $navigationLabel = 'Users & Borrow';
+    protected static ?int $navigationSort = 1; 
 
     public static function form(Form $form): Form
     {
