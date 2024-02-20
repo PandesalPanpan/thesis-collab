34fef699a4ce875d1031258efa35519856bee159
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2024-02-20 10:42:00 +0800

    customized activityresource & default traits of spatie-activitylog
    - Customized the default Activity resource
    
    - EquipmentResource uses Traits LogActivity to insert logs
    
    - filament-logger configuration are customized to find the new ActivityResource

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 //use Spatie\MediaLibrary\MediaCollections\Models\Media;
 //use Spatie\MediaLibrary\HasMedia;
 //use Spatie\MediaLibrary\InteractsWithMedia;
@@ -12,10 +14,17 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Equipment extends Model //implements HasMedia
 {
     use HasFactory;
+    use LogsActivity;
     //use InteractsWithMedia;
     protected $fillable = [
         'name','user_id'
     ];
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['name','user.name','barcode','rfid'])
+            ->useLogName('Inventory');
+    }
     // public function registerMediaConversions(Media $media = null): void
     // {
     //     $this->addMediaConversion('thumb')
---
+++ config/filament-logger.php
@@ -0,0 +1,48 @@
+<?php
+return [
+    'datetime_format' => 'd/m/Y H:i:s',
+    'date_format' => 'd/m/Y',
+
+    'activity_resource' => App\Filament\Resources\ActivityResource::class,//\Z3d0X\FilamentLogger\Resources\ActivityResource::class,
+
+    'resources' => [
+        'enabled' => false, // main logger
+        'log_name' => 'Resource',
+        'logger' => \Z3d0X\FilamentLogger\Loggers\ResourceLogger::class, //App\Filament\Moderator\Loggers\ModelResourceLogger::class, //
+        'color' => 'success',
+        'exclude' => [
+            //App\Filament\Resources\UserResource::class,
+        ],
+    ],
+
+    'access' => [
+        'enabled' => true,
+        'logger' => \Z3d0X\FilamentLogger\Loggers\AccessLogger::class,
+        'color' => 'danger',
+        'log_name' => 'Access',
+    ],
+
+    'notifications' => [
+        'enabled' => true,
+        'logger' => \Z3d0X\FilamentLogger\Loggers\NotificationLogger::class,
+        'color' => null,
+        'log_name' => 'Notification',
+    ],
+
+    'models' => [
+        'enabled' => true,
+        'log_name' => 'Model',
+        'color' => 'warning',
+        'logger' => \Z3d0X\FilamentLogger\Loggers\ModelLogger::class,
+        'register' => [
+            //App\Models\User::class,
+        ],
+    ],
+
+    'custom' => [
+        [
+            'log_name' => 'Inventory',
+            'color' => 'primary',
+        ]
+    ],
+];
