a760f50efe1541ba1f8c92d775e5e7c80a27041f
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2024-02-22 12:02:36 +0800

    barcodeview and public table updated design by jamaica solina

--- resources/views/livewire/list-equipments.blade.php
+++ resources/views/livewire/list-equipments.blade.php
@@ -1,16 +1,30 @@
-<div class="wrapper w-full md:max-w-5xl mx-auto pt-20 px 4">
-    <h1 class="text-xl font-medium">Laboratory Equipments</h1>
-    <!-- If ever nasa merge conflict, delete this, prioritize yung kay Jam
-    pero I change ung href ni jam into "/moderator" din katulad nung nandito -->
-    <x-filament::button
-        href="/moderator"
-        tag="a"
-    >
-        Access Panel
-    </x-filament::button>
-    
-    <section class="pt-4">
-        {{ $this->table }}
-    </section>
-    
+<div class="grid h-screen sm:grid-cols-1 md:flex md:grid-cols-2 pt-20">
+    <!-- Side Panel -->
+    <div class="flex-none w-1/3 bg-gray-200 px-6 py-8">
+        <div class="bg-red-800 text-white rounded-lg shadow-md p-4 flex flex-col justify-center items-center md:h-screen ">
+            <!-- Image -->
+            <div class="relative flex justify-center items-center mt-8 gap-x-3">
+                <img class="object-cover object-center rounded-full h-32 w-32 md:h-48 md:w-48" src="images.png" alt="image"/>
+            </div>
+            
+            <!-- Text -->
+            <div>
+                <h1 class="text-3xl md:text-3xl font-semibold mb-4">Laboratory Equipment</h1>
+            </div>
+        </div>
+    </div>
+
+    <!-- Main Content -->
+    <div class="main-content bg-white rounded-lg shadow-md p-6 md:p-10 overflow-x-auto w-full">
+        <section>
+            {{ $this->table }}
+        </section>
+    </div>
+
+    <!-- Footer -->
+    <div class="bg-white p-8 flex justify-end items-end rounded-full shadow-md hover:text-center">
+        <a href="/moderator" class="text-gray-200 underline">
+            Access Panel
+        </a>
+    </div>
 </div>
