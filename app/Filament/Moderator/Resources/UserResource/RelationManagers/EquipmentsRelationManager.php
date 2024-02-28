6f4fa2f8f8b29239d95cdf1e8729b3a48aacea5c
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2024-02-28 18:01:26 +0800

    major commit for added borrowing
    - Added new fields for borrowing
    
    - Borrowing assosciatesa and also fill up other fields related to borrowing
    
    - Custom Action for Dissasocating/Returning of items that nulls proper fields

--- app/Filament/Moderator/Resources/UserResource/RelationManagers/EquipmentsRelationManager.php
+++ app/Filament/Moderator/Resources/UserResource/RelationManagers/EquipmentsRelationManager.php
@@ -5,13 +5,18 @@ namespace App\Filament\Moderator\Resources\UserResource\RelationManagers;
 use App\Models\Equipment;
 use App\Models\User;
 use Filament\Forms;
+use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Actions\Action;
 use Filament\Tables\Actions\AssociateAction;
 use Illuminate\Support\Facades\DB;
 
@@ -48,7 +53,8 @@ class EquipmentsRelationManager extends RelationManager
             ->headerActions([
                 Tables\Actions\Action::make('Borrow')
                     ->form([
-                        Forms\Components\Select::make('name')
+                        Select::make('name')
+                            ->label('Name of Equipment to be borrowed')
                             ->multiple()
                             ->helperText('Scan Barcode or manually type')
                             ->searchable()
@@ -59,23 +65,19 @@ class EquipmentsRelationManager extends RelationManager
                             ->limit(50)
                             ->pluck('name','id')
                             ->toArray())
-                            ->preload(),
-                        Forms\Components\Textarea::make('borrow_purpose')
+                            ->preload()
+                            ->required(),
+                        Textarea::make('borrow_purpose')
                             ->label("Purpose for borrowing")
-                            ->required()
+                            ->required(),
+                        TextInput::make('noted_instructor')
+                            ->label("Noted by Instructor"),
+                        DateTimePicker::make('borrow_date_return_deadline')
+                            ->format('Y-m-d H:i:s')
+                            ->seconds(false)
                             ])
-                            // ->options(Equipment::query()
-                            //     ->whereNull('user_id')
-                            //     ->pluck('name','barcode')
 
-                            //     ->options(Equipment::query()
-                            //         ->select([
-                            //             DB::raw("CONCAT(name, ' ', barcode) as namecode"), 'barcode',
-                            //         ])
-                            //         ->whereNull('user_id')
-                            //         ->pluck('namecode','barcode')
-                            //         ->toArray())
-                            // ->searchable()
+                        
 
 
                     ->action(function (array $data, Equipment $equipment): void{
@@ -83,10 +85,16 @@ class EquipmentsRelationManager extends RelationManager
                             $user = $this->getOwnerRecord();
                             // ddd($data);
                             // ddd($data["name"]);
-                            foreach ($data["name"] as $value){
+                            // TODO: Add Dates
+                            //ddd($data);
+                            foreach ($data["name"] as $value){ //$data['name'] is primary id
                                 $equipment = Equipment::whereIn('id', [$value])->first();
                                 $equipment->user()->associate($user);
                                 $equipment->borrow_purpose = $data["borrow_purpose"];
+                                $equipment->borrow_date_start = now();
+                                $equipment->borrow_date_return_deadline = $data['borrow_date_return_deadline'];
+                                $equipment->noted_instructor = $data['noted_instructor'];
+                                $equipment->increment('borrowed_count');
                                 $equipment->save();
                             }
                         });
@@ -101,15 +109,50 @@ class EquipmentsRelationManager extends RelationManager
                 or sa middleware where kapag cinofirm don na lalabas ung fingerprint scanner then verifies
                 */
             ])
-            // TODO: Possibly need to customize the disassociate to remove the borrow purpose
             ->actions([
-                Tables\Actions\DissociateAction::make(),
+                //Tables\Actions\DissociateAction::make(),
+                Action::make('Return')
+                    ->color("success")
+                    ->icon('heroicon-o-hand-raised')
+                    ->requiresConfirmation()
+                    ->successNotificationTitle('Successfully Returned')
+                    // i need to get the relationship first?
+                    ->action(function (Equipment $record) {
+                        DB::transaction(function () use ($record){
+                            $record->user()->dissociate();
+                            $record->borrow_last_returned = now();
+                            $record->borrow_purpose = null;
+                            $record->borrow_date_start = null;
+                            $record->borrow_date_return_deadline = null;
+                            $record->noted_instructor = null;
+                            $record->save(); 
+                        });
+                    }),
                 // Dito rin jocel ikakabit fingerprint
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DissociateBulkAction::make(),
-                ]),
+                //Tables\Actions\BulkActionGroup::make([
+                    //Tables\Actions\DissociateBulkAction::make(),
+                    Tables\Actions\BulkAction::make('Return')
+                        ->label('Return Selected')
+                        ->deselectRecordsAfterCompletion()
+                        ->requiresConfirmation()
+                        ->icon('heroicon-o-hand-raised')
+                        ->action(function (Collection $records){
+                            $records->each(function (Equipment $record): void {
+                                DB::transaction(function () use ($record) {
+                                    $record->user()->dissociate();
+                                    $record->borrow_last_returned = now();
+                                    $record->borrow_purpose = null;
+                                    $record->borrow_date_start = null;
+                                    $record->borrow_date_return_deadline = null;
+                                    $record->noted_instructor = null;
+                                    $record->save(); 
+                            });
+                        });
+                            
+                        })
+                //]),
             ]);
     }
 }
