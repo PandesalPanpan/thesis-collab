548d971fe9642fd44071a995d7167d6a097db252
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2024-03-05 08:43:33 +0800

    Clean up code

--- app/Filament/Moderator/Resources/UserResource/RelationManagers/EquipmentsRelationManager.php
+++ app/Filament/Moderator/Resources/UserResource/RelationManagers/EquipmentsRelationManager.php
@@ -3,21 +3,17 @@
 namespace App\Filament\Moderator\Resources\UserResource\RelationManagers;
 
 use App\Models\Equipment;
-use App\Models\User;
 use Filament\Forms;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Tables\Actions\Action;
-use Filament\Tables\Actions\AssociateAction;
 use Illuminate\Support\Facades\DB;
 use Milon\Barcode\DNS1D;
 use Spatie\Activitylog\Models\Activity;
@@ -90,10 +86,6 @@ class EquipmentsRelationManager extends RelationManager
                             ->format('Y-m-d H:i:s')
                             ->seconds(false)
                             ])
-
-                        
-
-
                     ->action(function (array $data, Equipment $equipment): void{
                         DB::transaction(function () use ($data){
                             $user = $this->getOwnerRecord();
@@ -104,13 +96,11 @@ class EquipmentsRelationManager extends RelationManager
                                 $equipment_original = $equipment->getOriginal();
                                 $equipment->user()->associate($user);
                                 $equipment->borrow_purpose = $data["borrow_purpose"];
-                                //$date = Carbon::now();
                                 $equipment->borrow_date_start = now()->format('Y-m-d H:i:s');
                                 $equipment->borrow_date_return_deadline = $data['borrow_date_return_deadline'];
                                 $equipment->noted_instructor = $data['noted_instructor'];
                                 $equipment->increment('borrowed_count');
                                 $equipment->save();   
-                                // ddd($equipment->getChanges());
                                 activity()
                                     ->causedBy(auth()->user()) // Assuming you have user authentication       
                                     ->withProperties([
@@ -122,32 +112,19 @@ class EquipmentsRelationManager extends RelationManager
                                     ->useLog('Borrow')
                                     ->on($equipment)
                                     ->log('borrow equipment');
-                                     // Log the changes made to the model
                             }
                             LogBatch::endBatch();
                         });
                         
                     })
-                /* 
-                Eto Jocel Cucustomize mo toh either gawa ka ng custom form field
-                or pwede ding laravel middleware, need aralin talaga
-                Sample Demonstration
-                Student Assistance clicks "Associate" -> Select Equipment 
-                -> Either open the fingerprint scanner app and verifies fingerprint na valid
-                or sa middleware where kapag cinofirm don na lalabas ung fingerprint scanner then verifies
-                */
             ])
             ->actions([
-                //Tables\Actions\DissociateAction::make(),
                 Action::make('Return')
                     ->color("success")
                     ->icon('heroicon-o-hand-raised')
                     ->requiresConfirmation()
                     ->successNotificationTitle('Successfully Returned')
-                    // i need to get the relationship first?
                     ->action(function (Equipment $record) {
-                        //ddd("Test");
-                        
                         DB::transaction(function () use ($record){
                             LogBatch::startBatch();
                             LogBatch::setBatch($this->getBatchUuid($record));
@@ -157,7 +134,6 @@ class EquipmentsRelationManager extends RelationManager
                         });
                         
                     }),
-                // Dito rin jocel ikakabit fingerprint
             ])
             ->bulkActions([
                 //Tables\Actions\BulkActionGroup::make([
@@ -169,36 +145,14 @@ class EquipmentsRelationManager extends RelationManager
                         ->icon('heroicon-o-hand-raised')
                         ->action(function (Collection $records){
                             DB::transaction(function () use ($records){
-                                //ddd($records); // Items with array of 1 => model/record, 2 => model/record
-                                // Create a method for getting the uuid of the first record
-                                
-                                
-                                
                                 LogBatch::startBatch();
                                 LogBatch::setBatch($this->getBatchUuid($records[0]));
-                                
                                 $records->each(function (Equipment $record): void {
                                     $this->dissociateAndLog($record);
                                 });
                                 LogBatch::endBatch();
                             });
                         })
-                                
-                        // ->action(function (Collection $records){
-                        //     $records->each(function (Equipment $record): void {
-                        //         DB::transaction(function () use ($record) {
-                        //             $record->user()->dissociate();
-                        //             $record->borrow_last_returned = now();
-                        //             $record->borrow_purpose = null;
-                        //             $record->borrow_date_start = null;
-                        //             $record->borrow_date_return_deadline = null;
-                        //             $record->noted_instructor = null;
-                        //             $record->save(); 
-                        //     });
-                        // });
-                            
-                        // })
-                //]),
             ]);
     }
 
