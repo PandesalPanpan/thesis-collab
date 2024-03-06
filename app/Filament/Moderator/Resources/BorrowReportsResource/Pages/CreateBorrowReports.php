c83f929be0604f2e51409690d2a795b8034edb2f
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2024-03-06 10:03:02 +0800

    Borrowed Reports for searching using batches

---
+++ app/Filament/Moderator/Resources/BorrowReportsResource/Pages/CreateBorrowReports.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Moderator\Resources\BorrowReportsResource\Pages;
+
+use App\Filament\Moderator\Resources\BorrowReportsResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBorrowReports extends CreateRecord
+{
+    protected static string $resource = BorrowReportsResource::class;
+}
